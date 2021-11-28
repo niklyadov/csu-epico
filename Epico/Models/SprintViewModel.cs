0d91b0999e63fd8012e0c7bdee49ee0756f83c26
Сергей Крутолевич <wither_87@mail.ru>
2021-11-28 13:48:16 +0500

    Added add feature to sprint page and logic, fix

---
+++ Epico/Models/SprintViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Epico.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epico.Models
+{
+    public class SprintViewModel
+    {
+        public int SprintId { get; set; }
+
+        public int ProjectId { get; set; }
+        public List<Sprint> Sprints { get; set; }
+            = new List<Sprint>();
+    }
+}
