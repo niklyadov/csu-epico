7b5a67a42f75a8436600a841919f91a2e4cd916a
Сергей Крутолевич <wither_87@mail.ru>
2021-12-10 11:25:04 +0500

    fix tabs

--- Epico/wwwroot/js/site.js
+++ Epico/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openInformation(evt, contentId) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tabs__button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active tabs__button-active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(contentId).style.display = "block";
+    evt.currentTarget.className += " active tabs__button-active";
+}
